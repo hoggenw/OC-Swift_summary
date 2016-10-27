0c5c0b11a8d055f482d61e6682f7b313e845c145
dev-wangliugen <hoggenw@gmail.com>
2016-10-27 14:53:32 +0800

    [add]Swift creat

---
+++ SWIFT/SWift/SWift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SWift
+//
+//  Created by 王留根 on 16/10/27.
+//  Copyright © 2016年 ios-mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
