9276fd4bb9ffa7da2badd15ef9cabe584bae72fe
dev-wangliugen <253192463@qq.com>
2016-11-12 19:13:54 +0800

    [add]update

--- SWIFT/SWift/SWift/VerifyCodeButton.swift
+++ SWIFT/SWift/SWift/VerifyCodeButton.swift
@@ -71,6 +71,73 @@ class VerifyCodeButton: UIButton {
             context?.strokePath();
             
         }
+        //MARK:获取本机ip
+        func getIPAddress() ->String {
+            var addresses = "error"
+            
+            // Get list of all interfaces on the local machine:
+            var ifaddr :  UnsafeMutablePointer<ifaddrs>? = nil
+            var temp_addr :  UnsafeMutablePointer<ifaddrs>? = nil
+            var success :Int = 0
+            success = Int(getifaddrs(&ifaddr))
+            if success == 0 {
+                temp_addr = ifaddr
+                while temp_addr != nil {
+                    //==============================================================
+                    //                let flags = Int32((temp_addr?.pointee.ifa_flags)!)
+                    //                var addr = temp_addr?.pointee.ifa_addr.pointee
+                    //
+                    //                // Check for running IPv4, IPv6 interfaces. Skip the loopback interface.
+                    //                if (flags & (IFF_UP|IFF_RUNNING|IFF_LOOPBACK)) == (IFF_UP|IFF_RUNNING) {
+                    //                    if addr?.sa_family == UInt8(AF_INET) || addr?.sa_family == UInt8(AF_INET6) {
+                    //
+                    //                        // Convert interface address to a human readable string:
+                    //                        var hostname = [CChar]()
+                    //                        if (getnameinfo(&addr!, socklen_t((addr?.sa_len)!), &hostname, socklen_t(hostname.count),nil, socklen_t(0), NI_NUMERICHOST) == 0) {
+                    //                            if let address = String(cString: hostname, encoding: .utf8) {
+                    //                                addresses.append(address)
+                    //                            }
+                    //                        }
+                    //                    }
+                    //                }
+                    
+                    // ================================================================
+                    if (Int32((temp_addr?.pointee.ifa_addr.pointee.sa_family)!) == AF_INET) {
+                        
+                        if (String(cString: (temp_addr?.pointee.ifa_name)!, encoding: .utf8) == "en0") {
+                            
+                            print("temp_addr->ifa_addr = \(temp_addr?.pointee.ifa_addr.pointee.sa_data),\(temp_addr?.pointee.ifa_addr.pointee.sa_family),\(temp_addr?.pointee.ifa_addr.pointee.sa_len)")
+                            //                        let sockaddrIn : sockaddr_in = sockaddr_in(sin_len: (temp_addr?.pointee.ifa_addr.pointee.sa_len)!, sin_family: (temp_addr?.pointee.ifa_addr.pointee.sa_family)!, sin_port: 0, sin_addr: in_addr(s_addr:0), sin_zero: (((temp_addr?.pointee.ifa_addr.pointee.sa_data)?.0)!,((temp_addr?.pointee.ifa_addr.pointee.sa_data)?.1)!,((temp_addr?.pointee.ifa_addr.pointee.sa_data)?.2)!,((temp_addr?.pointee.ifa_addr.pointee.sa_data)?.3)!,((temp_addr?.pointee.ifa_addr.pointee.sa_data)?.4)!,((temp_addr?.pointee.ifa_addr.pointee.sa_data)?.5)!,((temp_addr?.pointee.ifa_addr.pointee.sa_data)?.6)!,((temp_addr?.pointee.ifa_addr.pointee.sa_data)?.7)!))
+                            let fucn:UInt8  = UInt8(((temp_addr?.pointee.ifa_addr.pointee.sa_data)?[2])! < 0 ? (INT8_MAX + INT8_MAX + ((temp_addr?.pointee.ifa_addr.pointee.sa_data)?.2)!) : ((temp_addr?.pointee.ifa_addr.pointee.sa_data)?.2)!)
+                            let fucn2:UInt8  = UInt8(((temp_addr?.pointee.ifa_addr.pointee.sa_data)?.3)!)
+                            let fucn3:UInt8  = UInt8(((temp_addr?.pointee.ifa_addr.pointee.sa_data)?.4)!)
+                            let fucn4:UInt8  = UInt8(((temp_addr?.pointee.ifa_addr.pointee.sa_data)?.5)!)
+                            let sting: String = "\(fucn)\(fucn2)\(fucn3)\(fucn4)"
+                            let sin_addr :UInt32 = UInt32(sting)!
+                            
+                            addresses = String(cString: inet_ntoa(in_addr(s_addr:sin_addr)))
+                        }
+                        
+                    }
+                    
+                    temp_addr = temp_addr?.pointee.ifa_next
+                }
+                
+                
+            }
+            
+            
+            
+            print("ip ==== \(addresses)")
+            return addresses
+            
+        }
+        
+        
+        
+        
+        
+    }
        
     }
     
