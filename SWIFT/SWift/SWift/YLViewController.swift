d267407f9a979338f5889811e96f03919ec9d3a5
dev-wangliugen <253192463@qq.com>
2016-12-17 08:46:48 +0800

    about swift

---
+++ SWIFT/SWift/SWift/YLViewController.swift
@@ -0,0 +1,61 @@
+//
+//  YLViewController.swift
+//  SWift
+//
+//  Created by 王留根 on 16/12/16.
+//  Copyright © 2016年 ios-mac. All rights reserved.
+//
+
+import UIKit
+
+class YLViewController: UIViewController {
+    
+    //MARK:Public Property
+    
+    
+    
+    //MARK:Private Property
+    
+    
+    
+    //MARK:Public Methods
+    
+    
+    
+    //MARK:Override Methods
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    //MARK:User Events
+    
+    
+    //MARK:Private Methods
+
+}
+
+//MARK:Extension Delegate or Protocol
+
+
+
+
+
