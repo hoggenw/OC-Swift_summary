f18e0d4149c8e277f2429bca09c3b12e985ad138
hoggenw <253192463@qq.com>
2018-01-13 19:27:55 +0800

    no message

---
+++ YLViewController.xctemplate/UIViewControllerObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,36 @@
+//___FILEHEADER___
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+
+#pragma mark - Override Methods
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+#pragma mark - Public Methods
+
+
+#pragma mark - Events
+
+
+#pragma mark - Private Methods
+
+
+#pragma mark - Extension Delegate or Protocol
+
+@end
---
+++ YLViewController.xctemplate/UIViewControllerSwift/___FILEBASENAME___.swift
@@ -0,0 +1,54 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+// @class ___FILEBASENAMEASIDENTIFIER___
+// @abstract <#类的描述#>
+// @discussion <#类的功能#>
+//
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
+    
+    //MARK:Public Property
+    
+    
+    
+    //MARK:Private Property
+    
+    
+    
+    //MARK:Override Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hidesBottomBarWhenPushed = true;
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+    }
+    //MARK:Public Methods
+    
+    //MARK: Events
+
+    //MARK:Private Methods
+    
+    
+    
+    
+}
+
+//MARK:Extension Delegate or Protocol
+
